4b3e2f0568a4f912efb42ae1244807efaa8af2bb
Ujwal Raina <ujwalraina18@gmail.com>
2021-10-03 19:49:38 +0530

    make home page

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const expressLayout=require('express-ejs-layouts');
 const path=require('path');
 const PORT=process.env.PORT||3300;
 
+
+
+//Assets
+app.use(express.static('public'))
+
 app.get('/',(req,res)=>{
     res.render("home");
 });
